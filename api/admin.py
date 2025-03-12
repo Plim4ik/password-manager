6ba04d5acaed0e279cb85f9c8e551af475926ceb
HiBlakk <bronzakatana@gmail.com>
2025-03-12 15:59:00 +0300

    ✨ (admin.py): update PasswordEntryAdmin to display encrypted_password instead of password The change in the list_display property of the PasswordEntryAdmin class enhances security by ensuring that sensitive information, such as the password, is not displayed in the admin interface. Instead, it now shows the encrypted_password, which is a safer representation of the data.

--- api/admin.py
+++ api/admin.py
@@ -4,4 +4,4 @@ from .models import PasswordEntry
 # Register your models here.
 @admin.register(PasswordEntry)
 class PasswordEntryAdmin(admin.ModelAdmin):
-    list_display = ['service_name', 'password']
+    list_display = ['service_name', 'encrypted_password']
