bf33ff1f5f336344fde7df08ae66c86091e1ddf9
HiBlakk <bronzakatana@gmail.com>
2025-03-12 15:58:50 +0300

    ✨ (migrations, models): add initial migration for PasswordEntry model with encryption support The initial migration creates the PasswordEntry model, which includes fields for service name and encrypted password, along with a foreign key to the user model. This setup allows for secure storage of passwords. Additionally, methods for encrypting and decrypting passwords are added to enhance security, ensuring that sensitive data is handled appropriately. The migration is essential for establishing the database schema required for the application.

--- api/models.py
+++ api/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
+from cryptography.fernet import Fernet
+from django.conf import settings
+
 class PasswordEntry(models.Model):
     user = models.ForeignKey(
         User,
@@ -11,10 +14,7 @@ class PasswordEntry(models.Model):
         max_length=255,
         verbose_name=_('Имя сервиса')
     )
-    password = models.CharField(
-        max_length=255,
-        verbose_name=_('Пароль')
-    )
+    encrypted_password = models.BinaryField(verbose_name=_('Зашифрованный пароль'))
 
     class Meta:
         unique_together = ('user', 'service_name')
@@ -23,4 +23,23 @@ class PasswordEntry(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.service_name}"
+    
+    def encrypt_password(self, password: str):
+        if not settings.FERNET_KEY:
+            raise ValueError("🔴 Ошибка: FERNET_KEY отсутствует в настройках!")
+        fernet = Fernet(settings.FERNET_KEY.encode())
+        return fernet.encrypt(password.encode())
+
+
+    def decrypt_password(self):
+        """Расшифровываем пароль при запросе из БД"""
+        if not settings.FERNET_KEY:
+            raise ValueError("🔴 Ошибка: FERNET_KEY отсутствует в настройках!")
+
+        fernet = Fernet(settings.FERNET_KEY.encode())
+
+        encrypted_password = bytes(self.encrypted_password)
+
+        return fernet.decrypt(encrypted_password).decode()
+
     
